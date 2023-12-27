8725a0af30edf9ee57e2859fe3890ea2f0b79fbb
Fang_luo <fangluo531@gmail.com>
2023-12-27 07:44:10 -0500

    require sudo

--- intall.sh
+++ intall.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if the script is run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
 # Clean up: Delete the downloaded tar.gz and the extracted folder
 cleanup() {
     echo "Performing cleanup operations..."
