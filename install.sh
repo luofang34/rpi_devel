d9705ad22677c2fc850c5c748529d2cc831cafdd
Fang_luo <fangluo531@gmail.com>
2023-12-28 12:50:38 -0500

    ~

--- install.sh
+++ install.sh
@@ -205,7 +205,7 @@ then
     libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl
     rm -rf /root/.pyenv
     echo "Installing pyenv..."
-    sudo -u $real_user curl -s -S -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+    curl -s -S -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
     #curl https://pyenv.run | bash
 
     # Add pyenv to path
