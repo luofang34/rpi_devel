c2afe70d9525df109e4bfb5bc4aa1d57bb8ad58a
Fang_luo <fangluo531@gmail.com>
2023-12-28 23:37:17 -0500

    added tpu-setup.sh

---
+++ tpu-setup.sh
@@ -0,0 +1,16 @@
+# Detect real username
+if [ $SUDO_USER ]; then
+    real_user=$SUDO_USER
+else
+    real_user=$(whoami)
+fi
+
+
+#Add the Coral package repository to your apt-get distribution list by issuing the following commands:
+cd /home/$real_user/tflite1 
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install libedgetpu1-max
+wget https://dl.google.com/coral/canned_models/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite
+mv mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite Sample_TFLite_model/edgetpu.tflite
